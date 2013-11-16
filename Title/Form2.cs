5a6ed6e59c3ec931f8ebf868b1ff79db9fef12a8
khoksung <xpepex00701@hotmail.com>
2013-11-16 22:16:24 +0700

    Form1 to Form2
    ส่งฟอร์ม 1 ไป ฟอร์มสอง

---
+++ Title/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Title
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string str,string op2,string op3,string op4)
+        {
+            InitializeComponent();
+            label1.Text = str;
+            label2.Text = op2;
+            label3.Text = op3;
+            label4.Text = op4;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
